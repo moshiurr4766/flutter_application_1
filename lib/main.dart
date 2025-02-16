8207b62b9ea7db721162f0babea0704244e44451
moshiurr4766 <rahman15-4766@diu.edu.bd>
2025-02-16 10:33:30 +0600

    Task2

--- lib/main.dart
+++ lib/main.dart
@@ -13,67 +13,63 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text(
-                'Flutter Demo',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'IndieFlower',
-                  fontSize: 24.0,
-                ),
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'Flutter Demo',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'IndieFlower',
+                fontSize: 24.0,
               ),
-              backgroundColor: Colors.grey[800],
             ),
-            body: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('4.0',
-                      style: TextStyle(fontSize: 40.0, color: Colors.grey)),
-                  
-                  Row(
-                    children: [
-                    Icon(
-                      Icons.star,
-                      color: Colors.grey,
-                      size: 40.0,
-                    ),
-                    Icon(
-                      Icons.star,
-                      color: Colors.grey,
-                      size: 40.0,
-                    ),
-                    Icon(
-                      Icons.star,
-                      color: Colors.grey,
-                      size: 40.0,
-                    ),
-                    Icon(
-                      Icons.star,
-                      color: Colors.grey,
-                      size: 40.0,
-                    ),
-                    Icon(
-                      Icons.star_border,
-                      color: Colors.grey,
-                      size: 40.0,
-                    ),
-                    ],
-                  ),
-                  SizedBox(height: 20.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.zero,
-                    ),
-                    ),
-                    child: const Text('SUBMIT'),
-                  ),
-                ],
-              ),
-            )));
+            backgroundColor: Colors.grey[800],
+          ),
+          body: Container(
+            height: double.infinity,
+            color: Colors.grey[200],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.star,
+                  color: Colors.grey,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.grey,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.grey,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.grey,
+                  size: 40.0,
+                ),
+                Icon(
+                  Icons.star_border,
+                  color: Colors.grey,
+                  size: 40.0,
+                ),
+              ],
+            ),
+          ),
+          //SizedBox(height: 20.0),
+          // ElevatedButton(
+          //   onPressed: () {},
+          //   style: ElevatedButton.styleFrom(
+          //   shape: RoundedRectangleBorder(
+          //     borderRadius: BorderRadius.zero,
+          //   ),
+          //   ),
+          //   child: const Text('SUBMIT'),
+          // ),
+        ));
   }
 }
