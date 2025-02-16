baf556e8e864ac04a965ccdeb44e99cd56a59583
moshiurr4766 <rahman15-4766@diu.edu.bd>
2025-02-16 09:24:54 +0600

    initiate

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title:  Text(
+            'Flutter Demo',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'IndieFlower',
+              fontSize:24.0,
+              ),
+            ),
+          backgroundColor: Colors.grey[800],
+          
+        ),
+
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
