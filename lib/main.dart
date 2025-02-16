69ab4ded8c7f889ba0e6dad7e6348e68534ae524
moshiurr4766 <rahman15-4766@diu.edu.bd>
2025-02-16 12:52:56 +0600

    MainTaskDesign

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
+            centerTitle: true,
             title: Text(
               'Flutter Demo',
               style: TextStyle(
@@ -25,64 +26,104 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.grey[800],
           ),
           body: Container(
-            height: double.infinity,
-            width: double.infinity,
-            color: Colors.grey[200],
-            padding: EdgeInsets.all(20.0),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
+            height: 650.0,
+            padding: const EdgeInsets.all(16.0),
+            margin: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              color: const Color.fromARGB(255, 255, 253, 253),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 215, 212, 212),
+                  blurRadius: 5.0,
+                  spreadRadius: 1.0,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Column(
-                children: [
-                  Icon(Icons.account_circle, size: 50.0, color: Colors.grey[800]),
+                children: <Widget>[
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(
-                        Icons.account_circle,
-                        size: 50.0,
-                        color: Colors.grey[800],
+                      SizedBox(height: 40.0),
+                      Text(
+                        '50% OFF',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
+                      SizedBox(width: 180.0),
                       Icon(
-                        Icons.account_circle,
-                        size: 50.0,
-                        color: Colors.grey[800],
+                        Icons.favorite_border,
+                        color: Colors.grey,
+                        size: 35.0,
                       ),
                     ],
                   ),
+                  SizedBox(height: 20.0),
+                  Image.asset('assets/pho.png'),
+                  SizedBox(height: 20.0),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(
-                        Icons.account_circle,
-                        size: 50.0,
-                        color: Colors.grey[800],
-                      ),
-                      Icon(
-                        Icons.account_circle,
-                        size: 50.0,
-                        color: Colors.grey[800],
-                      ),
-                      Icon(
-                        Icons.account_circle,
-                        size: 50.0,
-                        color: Colors.grey[800],
+                      Text(
+                        'Redmi Note 4',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.w400,
+                        ),
                       ),
                     ],
-                  )
+                  ),
+                  SizedBox(height: 15.0),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'N: ',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24.0,
+                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '45,000',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 100.0),
+                      Text(
+                        '\$ ',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18.0,
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                      SizedBox(width: 5.0),
+                      Text(
+                        ' 55,000',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 18.0,
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
           ),
-          //SizedBox(height: 20.0),
-          // ElevatedButton(
-          //   onPressed: () {},
-          //   style: ElevatedButton.styleFrom(
-          //   shape: RoundedRectangleBorder(
-          //     borderRadius: BorderRadius.zero,
-          //   ),
-          //   ),
-          //   child: const Text('SUBMIT'),
-          // ),
         ));
   }
 }
